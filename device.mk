60bdbbd704d91dc87a92a62a9053d2c45f4c142d
alexandrusemina <alexandrusemina8@gmail.com>
2022-04-04 00:12:49 +0300

    fixed ram usage and app crashing

--- device.mk
+++ device.mk
@@ -12,7 +12,7 @@ TARGET_SCREEN_WIDTH := 1080
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.1-service.exynos7885
+    android.hardware.usb@1.3-service.samsung
 
 # Feature Permissions
 PRODUCT_COPY_FILES += \
@@ -20,8 +20,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.sensor.light.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.light.xml
 
 PRODUCT_PACKAGES += \
-   fstab.exynos7904 \
-   ueventd.exynos7885.rc
+   fstab.exynos7904
    
 PRODUCT_PACKAGES += \
    android.hardware.sensors@1.0-service
